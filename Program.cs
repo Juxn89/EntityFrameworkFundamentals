14c49d527aa6dbe05e7fc3f61c2d3a56f2a64e4e
juan.gomez <gomezj01@stepstone.com>
2022-05-23 15:57:20 -0600

    Using entity framework in memory.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using efFundamentals.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TasksDBContext>(p => p.UseInMemoryDatabase("TasksDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbConnection", async ([FromServices] TasksDBContext dbContext) => {
+    dbContext.Database.EnsureCreated();
+    return Results.Ok($"DB in memory: { dbContext.Database.IsInMemory() }");
+});
 
 app.Run();
