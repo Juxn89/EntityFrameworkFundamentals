7698101d4469d0e14e0ce25e7f058c2b73eb969a
juan.gomez <gomezj01@stepstone.com>
2022-05-24 11:20:50 -0600

    Adding seed data to models using migrations.

--- Context/TasksDBContext.cs
+++ Context/TasksDBContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using efFundamentals.Models;
+using efFundamentals.enums;
 
 namespace efFundamentals.Context;
 
@@ -11,8 +12,10 @@ public class TasksDBContext : DbContext {
             category.ToTable("Category");
             category.HasKey(k => k.Id);
             category.Property(p => p.Name).IsRequired().HasMaxLength(150);
-            category.Property(p => p.Description);
+            category.Property(p => p.Description).IsRequired(false);
             category.Property(p => p.Weight);
+
+            category.HasData(CategoriesSeed());
         });
 
         modelBuilder.Entity<Models.Task>(task => {
@@ -20,13 +23,56 @@ public class TasksDBContext : DbContext {
             task.HasKey(p => p.Id);
             task.HasOne(p => p.Category).WithMany(p => p.Tasks).HasForeignKey(p => p.CategoryId);
             task.Property(p => p.Title).IsRequired().HasMaxLength(200);
-            task.Property(p => p.Description);
+            task.Property(p => p.Description).IsRequired(false);
             task.Property(p => p.Priority);
             task.Property(p => p.CreationDate);
             task.Ignore(p => p.Summary);
+
+            task.HasData(TasksSeed());
         });
     }
 
     public DbSet<Models.Task> Tasks {get; set;}
     public DbSet<Category> Categories {get; set;}
+
+    #region Seed
+    private IEnumerable<Category> CategoriesSeed() {
+        List<Category> categoryList = new List<Category>();
+        categoryList.Add(new Category{ 
+            Id = Guid.Parse("10be4d76-8e50-4606-8628-ce9aede5cdf2"),
+            Name = "Pending activities",
+            Weight = 20
+        });
+
+        categoryList.Add(new Category{ 
+            Id = Guid.Parse("10be4d76-8e50-4606-8628-ce9aede5cdf3"),
+            Name = "Personal activities",
+            Weight = 50
+        });
+
+        return categoryList;
+    }
+    
+    private IEnumerable<Models.Task> TasksSeed() {
+        List<Models.Task> taskList = new List<Models.Task>();
+        taskList.Add(new Models.Task {
+            Id = Guid.Parse("10be4d76-8e50-4606-8628-ce9aede5cd10"),
+            CategoryId = Guid.Parse("10be4d76-8e50-4606-8628-ce9aede5cdf2"),
+            Title = "Paymente of public sercices",
+            CreationDate = DateTime.Now.Date,
+            Priority = Priority.Medium
+        });
+
+        taskList.Add(new Models.Task {
+            Id = Guid.Parse("10be4d76-8e50-4606-8628-ce9aede5cd20"),
+            CategoryId = Guid.Parse("10be4d76-8e50-4606-8628-ce9aede5cdf3"),
+            Title = "Finish watching movie on Netflix",
+            CreationDate = DateTime.Now.Date,
+            Priority = Priority.Low
+        });
+
+
+        return taskList;
+    }
+    #endregion
 }
---
+++ Migrations/20220524171647_InitialData.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkFundamentals.Migrations
+{
+    public partial class InitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Task",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Description", "Name", "Weight" },
+                values: new object[] { new Guid("10be4d76-8e50-4606-8628-ce9aede5cdf2"), null, "Pending activities", 20 });
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Description", "Name", "Weight" },
+                values: new object[] { new Guid("10be4d76-8e50-4606-8628-ce9aede5cdf3"), null, "Personal activities", 50 });
+
+            migrationBuilder.InsertData(
+                table: "Task",
+                columns: new[] { "Id", "CategoryId", "CreationDate", "Description", "Priority", "Title" },
+                values: new object[] { new Guid("10be4d76-8e50-4606-8628-ce9aede5cd10"), new Guid("10be4d76-8e50-4606-8628-ce9aede5cdf2"), new DateTime(2022, 5, 24, 0, 0, 0, 0, DateTimeKind.Local), null, 1, "Paymente of public sercices" });
+
+            migrationBuilder.InsertData(
+                table: "Task",
+                columns: new[] { "Id", "CategoryId", "CreationDate", "Description", "Priority", "Title" },
+                values: new object[] { new Guid("10be4d76-8e50-4606-8628-ce9aede5cd20"), new Guid("10be4d76-8e50-4606-8628-ce9aede5cdf3"), new DateTime(2022, 5, 24, 0, 0, 0, 0, DateTimeKind.Local), null, 0, "Finish watching movie on Netflix" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Task",
+                keyColumn: "Id",
+                keyValue: new Guid("10be4d76-8e50-4606-8628-ce9aede5cd10"));
+
+            migrationBuilder.DeleteData(
+                table: "Task",
+                keyColumn: "Id",
+                keyValue: new Guid("10be4d76-8e50-4606-8628-ce9aede5cd20"));
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: new Guid("10be4d76-8e50-4606-8628-ce9aede5cdf2"));
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: new Guid("10be4d76-8e50-4606-8628-ce9aede5cdf3"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Task",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
